d8d3545b770352b0ed2cdd4d1eac1c0c3ea27b8f
matheus3009 <matheussilvaborgessousa@gmail.com>
2024-05-14 17:25:31 -0300

    inicio php

---
+++ 01-hello-world.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 01 de PHP</title>
+</head>
+<body>
+    <h1>Experiências com PHP</h1>
+    <hr>
+     <!-- delimitadores de abertura/fechamento PHP -->
+        <?php
+        echo "Olá";
+        echo "<p>Senac Penha</p>";
+        ?>
+     <!-- delimitadores de abertura/fechamento PHP -->
+</body>
+</html>

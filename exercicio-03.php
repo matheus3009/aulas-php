3d778f13ff915bf096623f681d8645214ddaafa9
matheus3009 <matheussilvaborgessousa@gmail.com>
2024-05-21 14:14:05 -0300

    Update exercicio-03.php

--- exercicio-03.php
+++ exercicio-03.php
@@ -5,25 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 3</title>
     <style>
-        ol li{
-            list-style: decimal;
+        li:nth-child(odd){
+            border: solid black;
+            background-color: hotpink;
+            padding: 2px;
         }
     </style>
+
+
+
 </head>
 <body>
     <div>
     <h1>Exercício 3</h1>
     <hr>
     <?php
-    $meses = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro" ];
+    $meses = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro" ];?>
+   <ol>
 
-     foreach ($meses as $mes) {?>
-        <ol>
+   <?php
+     foreach ($meses as $mes) {
+        ?>
             <li ><?=$mes?></li>
-        </ol>
     <?php 
     };
-    ?>
+    ?>  
+    </ol>
     </div>
+
+
 </body>
 </html>
